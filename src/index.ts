341ebe0c889be9a87c79c9823ef7ca5ea7b18075
Clinton Selke <clinuxrulz@gmail.com>
2026-05-20 14:22:50 +1000

    starting to layout data structures for reactivity

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
+const NODE_FLAG_IN_QUEUE = 0b01;
+const NODE_FLAG_DIRTY    = 0b10;
+
 const NULL_REPR = 0xFFFFFFFF;
-const NODE_CALC_OFFSET = 0;
-const NODE_PARENT_OFFSET = 1;
-const NODE_PREV_OFFSET = 2;
-const NODE_NEXT_OFFSET = 3;
-const NODE_CHILDREN_HEAD = 4;
-const NODE_CHILDREN_TAIL = 5;
-const NODE_SIZE = 6;
+const NODE_FLAGS_OFFSET = 0;
+const NODE_CALC_OFFSET = 1;
+const NODE_VALUE_OFFSET = 2;
+const NODE_PARENT_OFFSET = 3;
+const NODE_PREV_OFFSET = 4;
+const NODE_NEXT_OFFSET = 5;
+const NODE_SUB_HEAD_OFFSET = 6;
+const NODE_SUB_TAIL_OFFSET = 7;
+const NODE_CLEANUP_OFFSET = 8;
+const NODE_SIZE = 9;
 
 let nodeMemoryReserveSize = 1000 * NODE_SIZE;
 let nodeMemorySize = 0;
@@ -13,6 +19,10 @@ let nodeMemory = new Uint32Array(nodeMemoryReserveSize);
 let nodeFreeTableReserveSize = 1000;
 let nodeFreeTableSize = 0;
 let nodeFreeTable = new Uint32Array(nodeFreeTableReserveSize);
+let owner: number | undefined = undefined;
+let observer: number | undefined = undefined;
+let queueHead: number | undefined = undefined;
+let queueTail: number | undefined = undefined;
 
 function allocNode(): number {
   if (nodeFreeTableSize > 0) {
@@ -61,4 +71,124 @@ function allocJsValue(jsValue: any): number {
 function freeJsValue(ptr: number) {
   jsValueTable[ptr] = undefined;
   freeJsValueTable.push(ptr);
+}
+
+type Accessor<A> = number & { __accessor: A, };
+type Signal<A> = number & { __signal: A, __accessor: A, };
+
+function node(
+  calcPtr: number,
+  valuePtr: number,
+): number {
+  let nodePtr = allocNode();
+  nodeMemory[nodePtr + NODE_FLAGS_OFFSET] = 0;
+  nodeMemory[nodePtr + NODE_CALC_OFFSET] = calcPtr;
+  nodeMemory[nodePtr + NODE_VALUE_OFFSET] = valuePtr;
+  nodeMemory[nodePtr + NODE_PARENT_OFFSET] = NULL_REPR;
+  nodeMemory[nodePtr + NODE_PREV_OFFSET] = NULL_REPR;
+  nodeMemory[nodePtr + NODE_NEXT_OFFSET] = NULL_REPR;
+  nodeMemory[nodePtr + NODE_SUB_HEAD_OFFSET] = NULL_REPR;
+  nodeMemory[nodePtr + NODE_SUB_TAIL_OFFSET] = NULL_REPR;
+  nodeMemory[nodePtr + NODE_CLEANUP_OFFSET] = NULL_REPR;
+  return nodePtr;
+}
+
+function enqueueNode(nodePtr: number) {
+  if (nodeMemory[nodePtr + NODE_FLAGS_OFFSET] & NODE_FLAG_IN_QUEUE) {
+    return;
+  }
+  nodeMemory[nodePtr + NODE_FLAGS_OFFSET] |= NODE_FLAG_IN_QUEUE;
+  if (queueHead === undefined) {
+    queueHead = nodePtr;
+    queueTail = nodePtr;
+  } else {
+    nodeMemory[queueTail! + NODE_NEXT_OFFSET] = nodePtr;
+    nodeMemory[nodePtr + NODE_PREV_OFFSET] = queueTail!;
+    queueTail = nodePtr;
+  }
+}
+
+function dequeueNode(nodePtr: number) {
+  if (nodeMemory[nodePtr + NODE_FLAGS_OFFSET] & ~NODE_FLAG_IN_QUEUE) {
+    return;
+  }
+  nodeMemory[nodePtr + NODE_FLAGS_OFFSET] ^= NODE_FLAG_IN_QUEUE;
+  let prevPtr = nodeMemory[nodePtr + NODE_PREV_OFFSET];
+  let nextPtr = nodeMemory[nodePtr + NODE_NEXT_OFFSET];
+  if (prevPtr !== NULL_REPR) {
+    nodeMemory[prevPtr + NODE_NEXT_OFFSET] = nextPtr;
+  }
+  if (nextPtr !== NULL_REPR) {
+    nodeMemory[nextPtr + NODE_PREV_OFFSET] = prevPtr;
+  }
+  if (queueHead === nodePtr) {
+    queueHead = nextPtr === NULL_REPR ? undefined : nextPtr;
+  }
+  if (queueTail === nodePtr) {
+    queueTail = prevPtr === NULL_REPR ? undefined : prevPtr;
+  }
+}
+
+function signal<A>(init: A): Signal<A> {
+  let nodePtr = node(
+    NULL_REPR,
+    allocJsValue(init),
+  );
+  return nodePtr as Signal<A>;
+}
+
+function memo<A>(fn: () => A): Accessor<A> {
+  let nodePtr = node(
+    allocJsValue(fn),
+    NULL_REPR,
+  );
+  let lastOwner = owner;
+  let lastObserver = observer;
+  try {
+    owner = nodePtr;
+    observer = nodePtr;
+    let initValue = fn();
+    nodeMemory[nodePtr + NODE_VALUE_OFFSET] = allocJsValue(initValue);
+  } finally {
+    owner = lastOwner;
+    observer = lastObserver;
+  }
+  return nodePtr as Accessor<A>;
+}
+
+function updateNode(nodePtr: number) {
+
+}
+
+function read<A>(a: Accessor<A>): A {
+  let nodePtr = a;
+  let valuePtr = nodeMemory[nodePtr + NODE_VALUE_OFFSET];
+  return jsValueTable[valuePtr] as A;
+}
+
+function write<A>(signal: Signal<A>, value: A) {
+  let valuePtr = nodeMemory[signal + NODE_VALUE_OFFSET];
+  jsValueTable[valuePtr] = value;
+}
+
+function cleanup(fn: () => void) {
+  if (owner === undefined) {
+    fn();
+    return;
+  }
+  let cleanupPtr = nodeMemory[owner + NODE_CLEANUP_OFFSET];
+  if (cleanupPtr === NULL_REPR) {
+    nodeMemory[owner + NODE_CLEANUP_OFFSET] = allocJsValue(fn);
+  } else {
+    let lastCleanup = jsValueTable[cleanupPtr];
+    if (cleanup instanceof Function) {
+      jsValueTable[cleanupPtr] = [
+        lastCleanup,
+        fn,
+      ];
+    } else {
+      // the cleanups must be an array if we reach here
+      (lastCleanup as any).push(fn);
+    }
+  }
 }
