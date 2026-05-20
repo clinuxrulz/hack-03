7f9ef6bc77ddfc2454f9d232d867788edb67aca3
Clinton Selke <clinuxrulz@gmail.com>
2026-05-20 16:24:36 +1000

    preparing Link(s) for managing subscribers / dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
-const NODE_FLAG_IN_QUEUE = 0b01;
-const NODE_FLAG_DIRTY    = 0b10;
+const NODE_FLAG_IN_QUEUE = 0b0001;
+const NODE_FLAG_DIRTY    = 0b0010;
+const NODE_VISITED       = 0b0100;
 
 const NULL_REPR = 0xFFFFFFFF;
 const NODE_FLAGS_OFFSET = 0;
 const NODE_CALC_OFFSET = 1;
 const NODE_VALUE_OFFSET = 2;
 const NODE_PARENT_OFFSET = 3;
-const NODE_PREV_OFFSET = 4;
-const NODE_NEXT_OFFSET = 5;
-const NODE_SUB_HEAD_OFFSET = 6;
-const NODE_SUB_TAIL_OFFSET = 7;
-const NODE_CLEANUP_OFFSET = 8;
-const NODE_SIZE = 9;
+const NODE_HEAP_PREV_OFFSET = 4;
+const NODE_HEAP_NEXT_OFFSET = 5;
+const NODE_DEPS_HEAD_OFFSET = 7;
+const NODE_DEPS_TAIL_OFFSET = 8;
+const NODE_SUBS_HEAD_OFFSET = 9;
+const NODE_SUBS_TAIL_OFFSET = 10;
+const NODE_CLEANUP_OFFSET = 11;
+const NODE_SIZE = 12;
+
+const LINK_DEP_OFFSET = 0;
+const LINK_SUB_OFFSET = 1;
+const LINK_NEXT_DEP_OFFSET = 2;
+const LINK_PREV_SUB_OFFSET = 3;
+const LINK_NEXT_SUB_OFFSET = 4;
+const LINK_SIZE = 5;
 
 let nodeMemoryReserveSize = 1000 * NODE_SIZE;
 let nodeMemorySize = 0;
@@ -19,6 +29,12 @@ let nodeMemory = new Uint32Array(nodeMemoryReserveSize);
 let nodeFreeTableReserveSize = 1000;
 let nodeFreeTableSize = 0;
 let nodeFreeTable = new Uint32Array(nodeFreeTableReserveSize);
+let linkMemoryReserveSize = 4000 * LINK_SIZE;
+let linkMemorySize = 0;
+let linkMemory = new Uint32Array(linkMemoryReserveSize);
+let linkFreeTableReserveSize = 4000;
+let linkFreeTableSize = 0;
+let linkFreeTable = new Uint32Array(linkFreeTableReserveSize);
 let owner: number | undefined = undefined;
 let observer: number | undefined = undefined;
 let queueHead: number | undefined = undefined;
@@ -54,6 +70,38 @@ function freeNode(ptr: number) {
   nodeFreeTable[nodeFreeTableSize++] = ptr;
 }
 
+function allocLink(): number {
+  if (linkFreeTableSize > 0) {
+    let ptr = linkFreeTable[--linkFreeTableSize];
+    return ptr;
+  }
+  if (linkMemorySize === linkMemoryReserveSize) {
+    let newReserveSize = linkFreeTableReserveSize << 1;
+    let newLinksMemory = new Uint32Array(newReserveSize);
+    for (let i = 0; i < linkMemorySize; ++i) {
+      newLinksMemory[i] = linkMemory[i];
+    }
+    linkMemory = newLinksMemory;
+    linkMemoryReserveSize = newReserveSize;
+  }
+  let ptr = linkMemorySize;
+  linkFreeTableSize += LINK_SIZE;
+  return ptr;
+}
+
+function freeLink(ptr: number) {
+  if (linkFreeTableSize === linkFreeTableReserveSize) {
+    let newReserveSize = linkFreeTableReserveSize << 1;
+    let newLinksFreeTable = new Uint32Array(newReserveSize);
+    for (let i = 0; i < linkFreeTableSize; ++i) {
+      newLinksFreeTable[i] = linkFreeTable[i];
+    }
+    linkFreeTable = newLinksFreeTable;
+    linkFreeTableReserveSize = newReserveSize;
+  }
+  linkFreeTable[linkFreeTableSize++] = ptr;
+}
+
 let jsValueTable: any[] = [];
 let freeJsValueTable: number[] = [];
 
@@ -85,10 +133,12 @@ function node(
   nodeMemory[nodePtr + NODE_CALC_OFFSET] = calcPtr;
   nodeMemory[nodePtr + NODE_VALUE_OFFSET] = valuePtr;
   nodeMemory[nodePtr + NODE_PARENT_OFFSET] = NULL_REPR;
-  nodeMemory[nodePtr + NODE_PREV_OFFSET] = NULL_REPR;
-  nodeMemory[nodePtr + NODE_NEXT_OFFSET] = NULL_REPR;
-  nodeMemory[nodePtr + NODE_SUB_HEAD_OFFSET] = NULL_REPR;
-  nodeMemory[nodePtr + NODE_SUB_TAIL_OFFSET] = NULL_REPR;
+  nodeMemory[nodePtr + NODE_HEAP_PREV_OFFSET] = NULL_REPR;
+  nodeMemory[nodePtr + NODE_HEAP_NEXT_OFFSET] = NULL_REPR;
+  nodeMemory[nodePtr + NODE_DEPS_HEAD_OFFSET] = NULL_REPR;
+  nodeMemory[nodePtr + NODE_DEPS_TAIL_OFFSET] = NULL_REPR;
+  nodeMemory[nodePtr + NODE_SUBS_HEAD_OFFSET] = NULL_REPR;
+  nodeMemory[nodePtr + NODE_SUBS_TAIL_OFFSET] = NULL_REPR;
   nodeMemory[nodePtr + NODE_CLEANUP_OFFSET] = NULL_REPR;
   return nodePtr;
 }
@@ -102,8 +152,8 @@ function enqueueNode(nodePtr: number) {
     queueHead = nodePtr;
     queueTail = nodePtr;
   } else {
-    nodeMemory[queueTail! + NODE_NEXT_OFFSET] = nodePtr;
-    nodeMemory[nodePtr + NODE_PREV_OFFSET] = queueTail!;
+    nodeMemory[queueTail! + NODE_HEAP_NEXT_OFFSET] = nodePtr;
+    nodeMemory[nodePtr + NODE_HEAP_PREV_OFFSET] = queueTail!;
     queueTail = nodePtr;
   }
 }
@@ -113,13 +163,13 @@ function dequeueNode(nodePtr: number) {
     return;
   }
   nodeMemory[nodePtr + NODE_FLAGS_OFFSET] ^= NODE_FLAG_IN_QUEUE;
-  let prevPtr = nodeMemory[nodePtr + NODE_PREV_OFFSET];
-  let nextPtr = nodeMemory[nodePtr + NODE_NEXT_OFFSET];
+  let prevPtr = nodeMemory[nodePtr + NODE_HEAP_PREV_OFFSET];
+  let nextPtr = nodeMemory[nodePtr + NODE_HEAP_NEXT_OFFSET];
   if (prevPtr !== NULL_REPR) {
-    nodeMemory[prevPtr + NODE_NEXT_OFFSET] = nextPtr;
+    nodeMemory[prevPtr + NODE_HEAP_NEXT_OFFSET] = nextPtr;
   }
   if (nextPtr !== NULL_REPR) {
-    nodeMemory[nextPtr + NODE_PREV_OFFSET] = prevPtr;
+    nodeMemory[nextPtr + NODE_HEAP_PREV_OFFSET] = prevPtr;
   }
   if (queueHead === nodePtr) {
     queueHead = nextPtr === NULL_REPR ? undefined : nextPtr;
